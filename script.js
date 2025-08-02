01a6e61bab5052dc70775705f045c6c6516654ed
Milav <milav.k.p@gmail.com>
2025-08-02 16:16:31 +0000

    Added javascript about handling the functions

---
+++ script.js
@@ -0,0 +1,160 @@
+const previousDisp = document.querySelector('.previous');
+const resultDisp = document.querySelector('.result');
+const buttons = document.querySelectorAll('.btn');
+const clear = document.querySelector('.clear');
+const del = document.querySelector('.delete');
+
+let firstNum ="";
+let secondNum ="";
+let operator ="";
+let result ="";
+let isError = false;
+let resultDisplay = false;
+
+buttons.forEach(button => {
+    button.addEventListener('click', () =>{
+        if(isError) return;
+        const value = button.textContent;
+
+        if(!isNaN(value) || value === "."){
+            handleNum(value);
+        }
+        else if (value === "+" || value === "-" || value === "*" || value === "/"){
+            handleOperator(value);
+        }
+        else if (value === "=" ){
+            handleEqual(value)
+        }
+    })
+});
+
+function handleNum(value){
+    if(value === "." && ((operator === "" && firstNum.includes(".")) || (operator !== "" && secondNum.includes(".")))){
+        return;
+    }
+    if(operator === ""){
+        firstNum += value;
+        resultDisp.textContent = firstNum;
+    }
+    else{
+        secondNum += value;
+        resultDisp.textContent = secondNum;
+    }
+}
+
+function handleOperator(value){
+    if(firstNum !=="" && secondNum !== ""){
+        switch(operator){
+            case "+":
+                result = parseFloat(firstNum)+parseFloat(secondNum);
+                break;
+            case "-":
+                result = parseFloat(firstNum)-parseFloat(secondNum);
+                break;
+            case "*":
+                result = parseFloat(firstNum)*parseFloat(secondNum);
+                 break;
+            case "/":
+                if(parseFloat(secondNum) === 0){
+                    result = "ERROR";
+                    
+                }
+                else{
+                    result = parseFloat(firstNum)/parseFloat(secondNum);
+                }
+                break;
+            default:
+                result = "Invalid";
+        }
+        firstNum = result;
+        secondNum = "";
+        resultDisp.textContent = "";        
+    }
+    operator = value;
+    previousDisp.textContent = `${firstNum} ${operator}`;
+}
+
+function handleEqual(value){
+    if(isError) return;
+    if(firstNum !== "" && secondNum !== "" && operator !== ""){
+        switch(operator){
+            case "+":
+                result = parseFloat(firstNum)+parseFloat(secondNum);
+                break;
+            case "-":
+                result = parseFloat(firstNum)-parseFloat(secondNum);
+                break;
+            case "*":
+                result = parseFloat(firstNum)*parseFloat(secondNum);
+                 break;
+            case "/":
+                if(parseFloat(secondNum) === 0){
+                    result = "ERROR";
+                    isError = true;
+                }
+                else{
+                    result = parseFloat(firstNum)/parseFloat(secondNum);
+                }
+                break;
+            default:
+                result = "Invalid";
+        }
+        previousDisp.textContent = `${firstNum} ${operator} ${secondNum}`;
+        resultDisp.textContent = result;
+        resultDisplay = true;
+
+        firstNum = result;
+        secondNum = "";
+        result = "";
+    }
+}
+
+clear.addEventListener('click',()=>{
+    handleClr();
+})
+
+del.addEventListener('click', () =>{
+    handleDelete();
+})
+
+function handleClr(){
+    firstNum = "";
+    secondNum = "";
+    operator = "";
+    resultDisp.textContent = "";
+    previousDisp.textContent = "";
+    isError = false;
+    resultDisplay = false;
+}
+
+function handleDelete(){
+    if(resultDisplay) return;
+
+    if(operator === "") {
+        firstNum = firstNum.slice(0, -1);
+        resultDisp.textContent = firstNum;
+    } else {
+        secondNum = secondNum.slice(0, -1);
+        resultDisp.textContent = secondNum;
+    }
+}
+
+document.addEventListener('keydown',(e)=>{
+    const key = e.key;
+    if(key === "Backspace"){
+        handleDelete();
+    }
+    if(!isNaN(key) || key === "."){
+        handleNum(key);
+    }
+    if (key === "+" || key === "-" || key === "*" || key === "/") {
+        handleOperator(key);
+    }
+    if (key === "Enter" || key === "=") {
+        handleEqual();
+    }
+    if (key === "Escape") {
+        handleClr();
+    }
+
+})
